80ea9626fa61236fffaf6afab36270a2e051e718
Elias Garza <elias.garza@kalypso.com>
2022-11-04 23:43:10 -0600

    clean

--- memory_manager.py
+++ memory_manager.py
@@ -1,5 +1,14 @@
 class memory_manager():
-    def __init__(self, const_table: dict, ):
-        pass
+    def __init__(self, const_table: dict):
+        self.gi_mem = []
+        self.gf_mem = []
+        self.li_mem = []
+        self.lf_mem = []
+        self.ti_mem = []
+        self.tf_mem = []
+        self.tb_mem = []
+        self.ci_mem = []
+        self.cf_mem = []
+        self.tp_mem = []
 
         
